84fad5b516c8a93581f3655b8ee357c86854f6ae
Alex Martishin <asmartishin@gmail.com>
2024-11-17 06:18:14 +0100

    Dockerise app

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,10 @@
 import "./index.css";
 import App from "./App";
-import { Provider } from "react-redux";
 import { createRoot } from "react-dom/client";
+import React from "react";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
